27ce140ea1a57d2411883b12974df464ea71c46f
przemyslawren <przemyslaw.ren@gmail.com>
2024-03-19 10:52:38 +0100

    add StandardInput component

--- src/components/inputs/StandardInput.tsx
+++ src/components/inputs/StandardInput.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import classNames from 'classnames';
 
-const StandardInput = () => {
+type Props = {
+  placeholder: string;
+  isLoading: boolean;
+  onChange: (value: string) => void;
+};
+
+const StandardInput: React.FC<Props> = ({
+  placeholder,
+  isLoading,
+  onChange,
+}) => {
+  const inputClassName = classNames([
+    'ui',
+    'fluid',
+    'icon',
+    'input',
+    isLoading && 'loading',
+  ]);
+
   return (
-    <div className="ui fluid icon input">
-      <input type={'text'}></input>
+    <div className={inputClassName}>
+      <input
+        type="text"
+        placeholder={placeholder}
+        onChange={(event) => onChange(event.target.value)}
+      />
+      {isLoading && <i className="search icon" />}
     </div>
   );
 };
